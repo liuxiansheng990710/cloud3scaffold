80aa481d2b414c78f02a7e539113d4168380b831
21 <1069823632@qq.com>
2024-09-25 10:03:56 +0800

    feat(undertow) ： 替换tomcat

---
+++ commons/commons-web/src/main/java/com/example/commons/web/autoconfigure/WebAutoConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.commons.web.autoconfigure;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.example.commons.web.undertow.UndertowServerFactoryCustomizer;
+
+import io.undertow.Undertow;
+
+/**
+ * <p>
+ * web自动配置类
+ * </p>
+ *
+ * @author : 21
+ * @since : 2024/9/24 17:25
+ */
+
+@ConditionalOnWebApplication
+@EnableWebMvc
+@Configuration
+public class WebAutoConfiguration {
+
+    /**
+     * web servlet undertow
+     *
+     * @return
+     */
+    @Bean
+    @ConditionalOnClass(Undertow.class)
+    public UndertowServerFactoryCustomizer undertowServerFactoryCustomizer() {
+        return new UndertowServerFactoryCustomizer();
+    }
+
+}
